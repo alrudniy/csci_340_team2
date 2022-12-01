13dccc1ec20f42a3e27de2a874548b32530fd50c
harrison <hneal@drew.edu>
2022-12-01 12:39:50 -0500

    init commit

--- src/tests/routes/test_user.py
+++ src/tests/routes/test_user.py
@@ -7,3 +7,11 @@ def test_get_user_auth_required(client: TestClient):
     response_data = response.json()
     assert "detail" in response_data
 
+# Test the Get user endpoint with invalid token - GET /api/v1/user
+
+def test_get_user_invalid_token(client: TestClient):
+    invalid_auth_header = {"Authorization": "Bearer wrong.header"}
+    response = client.get("/api/v1/user", headers=invalid_auth_header)
+    assert response.status_code == 401, response.text
+    response_data = response.json()
+    assert "detail" in response_data
